58cac1a9083dec042633c039089a2bd71fb2919a
shahjay00785 <shahjay00785@gmail.com>
2018-07-25 15:57:18 +0530

    using async pipe

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,5 @@
+import { Observable, Subject, ReplaySubject, from, of, range } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
@@ -8,10 +10,13 @@ import * as firebase from 'firebase';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  user: firebase.User;
+  //user: firebase.User;
+  //user: Observable<firebase.User>;
+  user$: Observable<firebase.User>;
 
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(user => console.log((this.user = user)));
+    //  afAuth.authState.subscribe(user => console.log((this.user = user)));
+    this.user$ = this.afAuth.authState;
   }
 
   ngOnInit() {}
